be8aca1b801d12ccc375698ba0435a5986631dfe
itsme.Rohan <jhambrohan44@gmail.com>
2018-02-25 14:16:39 +0530

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(user_name:'rohan0' , email:"rohan0@gmail.com",password:'rohan0',password_confirmation:'rohan0');
+User.create(user_name:'rohan1' , email:"rohan1@gmail.com",password:'rohan1',password_confirmation:'rohan1');
+User.create(user_name:'rohan2' , email:"rohan2@gmail.com",password:'rohan2',password_confirmation:'rohan2');
+User.create(user_name:'rohan3' , email:"rohan3@gmail.com",password:'rohan3',password_confirmation:'rohan3');
+User.create(user_name:'rohan4' , email:"rohan4@gmail.com",password:'rohan4',password_confirmation:'rohan4');
+User.create(user_name:'rohan5' , email:"rohan5@gmail.com",password:'rohan5',password_confirmation:'rohan5');
+User.create(user_name:'rohan6' , email:"rohan6@gmail.com",password:'rohan6',password_confirmation:'rohan6');
+User.create(user_name:'rohan7' , email:"rohan7@gmail.com",password:'rohan7',password_confirmation:'rohan7');
+puts 'trst user created'
