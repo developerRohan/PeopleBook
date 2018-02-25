90d901dce4f4bf1fd33ff3f2d16a5f1077725530
itsme.Rohan <jhambrohan44@gmail.com>
2018-02-25 16:33:11 +0530

    integrated friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   validates_presence_of :user_name
   validates_uniqueness_of :user_name
+
+  belongs_to :friendship,dependent: :destroy
+  belongs_to :inverse_friendship,class: 'friendship' , foreign_key: 'friend_id' ,dependent: :destroy
+
+  def request_friendship(target_user)
+  	self.friendships.create(friend: target_user)
+  end
+  
 end
