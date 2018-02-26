8169c3afd71528e244676f456d94374013226258
itsme.Rohan <jhambrohan44@gmail.com>
2018-02-26 17:54:28 +0530

    added different kinds of people using navbar designing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 	before_action :set_user , only: [:show]
   def show
 
   end
 
   def index
-	@users = User.all
+	 case params[:people]
+     when 'friends'
+      @users = current_user.active_friends
+     when 'pending'
+      @users = current_user.friend_requests_sent.map(&:friend)
+     when 'requests'
+      @users = current_user.friend_requests_recieved.map(&:user)
+     else
+      @users = User.all
+    end
   end
 
   private
